bef2fe9be755a60f75569fd612f8f30c6d4b0172
Joshua Ladepo <joshualadepo@gmail.com>
2026-09-23 15:00:25 +0100

    learning postgressSQL

--- app.py
+++ app.py
@@ -1,8 +1,66 @@
+import os
+from pathlib import Path
+
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
 
 
-from flask import Flask , jsonify , request 
+env_path = Path(__file__).with_name(".env")
+load_dotenv(env_path)
+
+database_url = os.getenv("DATABASE_URL")
+
+if not database_url:
+    raise RuntimeError("DATABASE_URL was not loaded")
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = database_url
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+    __tablename__ = "tasks"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
+
+@app.route("/api/db-test")
+def db_test():
+    tasks = Task.query.all()
+
+    return jsonify([
+        {
+            "id": task.id,
+            "title": task.title,
+            "completed": task.completed
+        }
+        for task in tasks
+    ])
+@app.route("/api/db-testing", methods = ["GET","POST"])
+def db_testing():
+    if request.method == "POST":
+        data = request.get_json()
+        if data is None:
+            return jsonify(error = "Invalid Request"), 400
+        title = data.get("title")
+        if  not isinstance(title,str) or not title.strip():
+            return jsonify(error = "Invalid format"),400
+        new_task = Task(title= title,completed = False)
+        db.session.add(new_task)
+        db.session.commit()
+        return jsonify({
+            "id" : new_task.id,
+             "title": new_task.title,
+             "completed" :new_task.completed
+        }), 201
+    
+    
+    tasks_list = []
+
 
 tasks = [
     {
@@ -76,6 +134,8 @@ def delete_task(task_id):
             tasks.remove(task)
             return jsonify(message="Task deleted") , 200
     return jsonify(error="Task not found"), 404
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
